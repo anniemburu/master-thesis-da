0f3a3845a7d4fe21078c71374a876a111aa828da
anniemburu <annemburu11@gmail.com>
2024-10-22 10:06:53 +0200

    Add bash scripts

---
+++ bash_scripts/rf.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --job-name=rf_v1
+#SBATCH --output=%x_%j.log
+#SBATCH --error=%x_%j.err
+#SBATCH --mail-user=mburu@uni-hildesheim.de
+#SBATCH --partition=STUD
+#SBATCH --gres=gpu:1
+
+cd ~Master_Thesis/master-thesis-da/experiments
+source activate MasterThesis
+srun python3 random_forest_exp.py
