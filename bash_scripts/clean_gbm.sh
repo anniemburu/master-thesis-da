a2c655c2161e04bf280b60fde2fa2211b16354d0
Anne Mburu <39761702+anniemburu@users.noreply.github.com>
2024-10-30 22:10:21 +0100

    Update clean_gbm.sh

--- bash_scripts/clean_gbm.sh
+++ bash_scripts/clean_gbm.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=dnn_gbm
+#SBATCH --job-name=gbm_cln
 #SBATCH --output=%x_%j.log
 #SBATCH --error=%x_%j.err
 #SBATCH --mail-user=mburu@uni-hildesheim.de
@@ -8,5 +8,6 @@
 
 
 cd ~/Master_Thesis/master-thesis-da/cleaning
-source activate MasterThesis
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate MasterThesis
 srun python3 cleaning_gbm.py
