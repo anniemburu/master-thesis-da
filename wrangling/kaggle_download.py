77754b44b829e598c17e25846bd14756111d7a7a
Anne Mburu <39761702+anniemburu@users.noreply.github.com>
2024-10-30 11:10:42 +0100

    Update kaggle_download.py

--- wrangling/kaggle_download.py
+++ wrangling/kaggle_download.py
@@ -4,3 +4,10 @@ import kagglehub
 path = kagglehub.dataset_download("annemburu/categorical-datasets-for-regression-problem")
 
 print("Path to dataset files:", path)
+
+#!mkdir -p ~/.kaggle
+#!mv kaggle.json ~/.kaggle/
+#!chmod 600 ~/.kaggle/kaggle.json
+
+#!kaggle datasets download annemburu/categorical-datasets-for-regression-problem
+#!unzip /content/mammals-image-classification-dataset-45-animals.zip -d /home/mburu/Master_Thesis/master-thesis-da
