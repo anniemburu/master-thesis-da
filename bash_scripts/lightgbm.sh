fcb63b9ecdf8bba00c5c48296d259709d9f7b284
anniemburu <annemburu11@gmail.com>
2024-11-01 13:07:22 +0100

    Added file

---
+++ bash_scripts/lightgbm.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --job-name=mult_lgb_v1
+#SBATCH --output=%x_%j.log
+#SBATCH --error=%x_%j.err
+#SBATCH --mail-user=mburu@uni-hildesheim.de
+#SBATCH --partition=STUD
+#SBATCH --gres=gpu:1
+
+
+cd ~/Master_Thesis/master-thesis-da/experiments
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate MasterThesis
+srun python3 lightgbm.py
