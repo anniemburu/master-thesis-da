eb3842b3cfc1f27c7ad56618f66bb7a60f626633
anniemburu <annemburu11@gmail.com>
2024-11-15 09:51:01 +0100

    Created the test vime shell script

---
+++ DNN_Trial/bash_scripts/vime_v1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --job-name=VIME_T
+#SBATCH --output=%x_%j.log
+#SBATCH --error=%x_%j.err
+#SBATCH --mail-user=mburu@uni-hildesheim.de
+#SBATCH --partition=STUD
+#SBATCH --gres=gpu:1
+
+
+cd ~/Master_Thesis/master-thesis-da/DNN_trial
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate TabSurvey
+srun python3 train.py --config/california_housing.yml --model_name VIME
