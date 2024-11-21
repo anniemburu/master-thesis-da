6b9372c83e4ef52cd2a524aa02472b552753deb4
anniemburu <annemburu11@gmail.com>
2024-11-21 16:59:38 +0100

    loads for boston, sensory and socmob

--- DNN_Trial/utils/load_data.py
+++ DNN_Trial/utils/load_data.py
@@ -13,123 +13,56 @@ def discretize_colum(data_clm, num_values=10):
     q = r // bin_sz
     return q
 
+# Get colnames
+def get_colnames(data, idx):
+    return data.columns[idx]
+
+#Get column index
+def get_colidx(data, colnames):
+    col_idx = []
+    for col in colnames:
+        col_idx.append(data.columns.get_loc(col))
+
+    return col_idx
 
 def load_data(args):
     print("Loading dataset " + args.dataset + "...")
 
-    if args.dataset == "CaliforniaHousing":  # Regression dataset
-        #X, y = sklearn.datasets.fetch_california_housing(return_X_y=True)
-	# See if we can print files/folders in this path
-
-        data = pd.read_csv("/home/mburu/Master_Thesis/master-thesis-da/DNN_Trial/california_housing.csv")
-
-        X = data.drop('target', axis=1)
-        y = data['target']
-
-        X = X.to_numpy()
-        y = y.to_numpy()
-
-    elif args.dataset == "Covertyßpe":  # Multi-class classification dataset
-        X, y = sklearn.datasets.fetch_covtype(return_X_y=True)
-        # X, y = X[:10000, :], y[:10000]  # only take 10000 samples from dataset
-
-    elif args.dataset == "KddCup99":  # Multi-class classification dataset with categorical data
-        X, y = sklearn.datasets.fetch_kddcup99(return_X_y=True)
-        X, y = X[:10000, :], y[:10000]  # only take 10000 samples from dataset
-
-        # filter out all target classes, that occur less than 1%
-        target_counts = np.unique(y, return_counts=True)
-        smaller1 = int(X.shape[0] * 0.01)
-        small_idx = np.where(target_counts[1] < smaller1)
-        small_tar = target_counts[0][small_idx]
-        for tar in small_tar:
-            idx = np.where(y == tar)
-            y[idx] = b"others"
-
-        # new_target_counts = np.unique(y, return_counts=True)
-        # print(new_target_counts)
-
-        '''
-        # filter out all target classes, that occur less than 100
-        target_counts = np.unique(y, return_counts=True)
-        small_idx = np.where(target_counts[1] < 100)
-        small_tar = target_counts[0][small_idx]
-        for tar in small_tar:
-            idx = np.where(y == tar)
-            y[idx] = b"others"
-
-        # new_target_counts = np.unique(y, return_counts=True)
-        # print(new_target_counts)
-        '''
-    elif args.dataset == "Adult" or args.dataset == "AdultCat":  # Binary classification dataset with categorical data, if you pass AdultCat, the numerical columns will be discretized.
-        url_data = "https://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.data"
-
-        features = ['age', 'workclass', 'fnlwgt', 'education', 'education_num', 'marital-status', 'occupation',
-                    'relationship', 'race', 'sex', 'capital-gain', 'capital-loss', 'hours-per-week', 'native-country']
-        label = "income"
-        columns = features + [label]
-        df = pd.read_csv(url_data, names=columns)
-
-        # Fill NaN with something better?
-        df.fillna(0, inplace=True)
-        if args.dataset == "AdultCat":
-            columns_to_discr = [('age', 10), ('fnlwgt', 25), ('capital-gain', 10), ('capital-loss', 10),
-                                ('hours-per-week', 10)]
-            for clm, nvals in columns_to_discr:
-                df[clm] = discretize_colum(df[clm], num_values=nvals)
-                df[clm] = df[clm].astype(int).astype(str)
-            df['education_num'] = df['education_num'].astype(int).astype(str)
-            args.cat_idx = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
-        X = df[features].to_numpy()
-        y = df[label].to_numpy()
-
-    elif args.dataset == "HIGGS":  # Binary classification dataset with one categorical feature
-        path = "/opt/notebooks/data/HIGGS.csv.gz"
-        df = pd.read_csv(path, header=None)
-        df.columns = ['x' + str(i) for i in range(df.shape[1])]
-        num_col = list(df.drop(['x0', 'x21'], 1).columns)
-        cat_col = ['x21']
-        label_col = 'x0'
-
-        def fe(x):
-            if x > 2:
-                return 1
-            elif x > 1:
-                return 0
-            else:
-                return 2
-
-        df.x21 = df.x21.apply(fe)
-
-        # Fill NaN with something better?
-        df.fillna(0, inplace=True)
-
-        X = df[num_col + cat_col].to_numpy()
-        y = df[label_col].to_numpy()
-
-    elif args.dataset == "Heloc":  # Binary classification dataset without categorical data
-        path = "heloc_cleaned.csv"  # Missing values already filtered
-        df = pd.read_csv(path)
-        label_col = 'RiskPerformance'
+    ####~~~~~~~~~~~~~~~~~~~~ SPECIFY HOW DATASETS ARE LOADED AND STUFF ~~~~~~~~~~~~~~~~~ 
+    if args.dataset == "Boston":
+        df = pd.read_csv('/home/mburu/Master_Thesis/master-thesis-da/datasets/531-boston/raw_data.csv')
+        #df = pd.read_csv('/Users/wambo/Desktop/Master Thesis/master-thesis-da/datasets/531-boston.csv')
+        label_col = "MEDV"
 
         X = df.drop(label_col, axis=1).to_numpy()
         y = df[label_col].to_numpy()
 
-    else:
-        raise AttributeError("Dataset \"" + args.dataset + "\" not available")
+    if args.dataset == "Socmob":
+        df = pd.read_csv('/home/mburu/Master_Thesis/master-thesis-da/datasets/541-socmob/raw_data.csv') #CLUSTER
+        #df = pd.read_csv('/Users/wambo/Desktop/Master Thesis/master-thesis-da/datasets/541-socmob.csv')
+        label_col = 'counts_for_sons_current_occupation'
+
+        X = df.drop(label_col, axis=1).to_numpy()
+        y = df[label_col].to_numpy()
+
+
+    if args.dataset == "Sensory":
+        df = pd.read_csv('/home/mburu/Master_Thesis/master-thesis-da/datasets/546-sensory/raw_data.csv') #CLUSTER
+        #df = pd.read_csv('/Users/wambo/Desktop/Master Thesis/master-thesis-da/datasets/546-sensory.csv')
+        label_col = 'Score'
+
+        X = df.drop(label_col, axis=1).to_numpy()
+        y = df[label_col].to_numpy()
+    #####################################################################################
 
     print("Dataset loaded!")
     print(X.shape)
 
-    # Preprocess target
+    # Preprocess target 
     if args.target_encode:
         le = LabelEncoder()
         y = le.fit_transform(y)
 
-        # Setting this if classification task
-        if args.objective == "classification":
-            args.num_classes = len(le.classes_)
-            print("Having", args.num_classes, "classes as target.")
 
     num_idx = []
     args.cat_dims = []
@@ -156,6 +89,10 @@ def load_data(args):
         new_x1 = ohe.fit_transform(X[:, args.cat_idx])
         new_x2 = X[:, num_idx]
         X = np.concatenate([new_x1, new_x2], axis=1)
+
+        #change the num of features after one hot encoding;
+        args.num_features = X.shape[1]
+        print(f"args.num_features: {args.num_features}")
         print("New Shape:", X.shape)
 
     return X, y
