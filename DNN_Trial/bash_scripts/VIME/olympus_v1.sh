bd5fe54709355d83c8592eb38f46babaf570899c
mburu <mburu@master.ismll.de>
2025-01-03 12:42:33 +0100

    Modified

--- DNN_Trial/bash_scripts/VIME/olympus_v1.sh
+++ DNN_Trial/bash_scripts/VIME/olympus_v1.sh
@@ -6,11 +6,11 @@
 #SBATCH --partition=STUD
 #SBATCH --gres=gpu:1
 
-CONFIGS=("config/boston.yml"
-        "config/socmob.yml"
-        "config/sensory.yml"
-        "config/moneyball.yml"
-        "config/black_friday.yml"
+CONFIGS=(config/boston.yml
+        config/socmob.yml
+       	config/sensory.yml
+        config/moneyball.yml
+        #config/black_friday.yml
           )
 
 for config in "${CONFIGS[@]}"; do
@@ -21,7 +21,7 @@ for config in "${CONFIGS[@]}"; do
     cd ~/Master_Thesis/master-thesis-da/DNN_Trial
     source ~/miniconda3/etc/profile.d/conda.sh
     conda activate TabSurvey
-    srun python3 train.py --config "$config" --model_name VIME --optimize_hyperparameters
+    srun python3 train.py --config "$config" --model_name VIME --optimize_hyperparameters --n_trials 100 --epochs 100
 done
 
 
