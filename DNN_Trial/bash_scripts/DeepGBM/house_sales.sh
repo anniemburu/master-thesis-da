95ecc64097b5a2fd12ec20ead35cbdc0b807498c
anniemburu <annemburu11@gmail.com>
2025-01-20 11:11:38 +0100

    Added extra scripts for failed experiments

---
+++ DNN_Trial/bash_scripts/DeepGBM/house_sales.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --job-name=DeepGBM_house_sales_V1
+#SBATCH --output=%x_%j.log
+#SBATCH --error=%x_%j.err
+#SBATCH --mail-user=mburu@uni-hildesheim.de
+#SBATCH --partition=STUD
+#SBATCH --gres=gpu:1
+
+cd ~/Master_Thesis/master-thesis-da/DNN_Trial
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate TabSurvey
+srun python3 train.py --config config/house_sales.yml --model_name DeepGBM --optimize_hyperparameters
