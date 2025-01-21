fb99229538cd2bbd7afa24f5808268999af557cd
Anne Mburu <39761702+anniemburu@users.noreply.github.com>
2025-01-21 11:54:52 +0100

    Update house_sales.sh

--- DNN_Trial/bash_scripts/NODE/house_sales.sh
+++ DNN_Trial/bash_scripts/NODE/house_sales.sh
@@ -9,6 +9,6 @@
 
 cd ~/Master_Thesis/master-thesis-da/DNN_Trial
 source ~/miniconda3/etc/profile.d/conda.sh
-conda activate TabSurvey
+conda activate Test4Node
 srun python3 train.py --config config/house_sales.yml --model_name NODE --optimize_hyperparameters --n_trials 93 --epochs 100
 
