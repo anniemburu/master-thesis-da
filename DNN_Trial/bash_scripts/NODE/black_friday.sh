8121c99014c5ed4ba6fc6de01451f1c907b092fb
Anne Mburu <39761702+anniemburu@users.noreply.github.com>
2025-01-21 11:54:00 +0100

    Update black_friday.sh

--- DNN_Trial/bash_scripts/NODE/black_friday.sh
+++ DNN_Trial/bash_scripts/NODE/black_friday.sh
@@ -9,6 +9,6 @@
 
 cd ~/Master_Thesis/master-thesis-da/DNN_Trial
 source ~/miniconda3/etc/profile.d/conda.sh
-conda activate TabSurvey
+conda activate Test4Node
 srun python3 train.py --config config/black_friday.yml --model_name NODE --optimize_hyperparameters --n_trials 18 --epochs 100
 
