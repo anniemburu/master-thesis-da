1f9861f32eeba2617acfd7b22f87b067273bdaf1
Anne Mburu <39761702+anniemburu@users.noreply.github.com>
2025-01-27 14:28:20 +0100

    Update odyssey_v1.sh

--- DNN_Trial/bash_scripts/CatBoost/odyssey_v1.sh
+++ DNN_Trial/bash_scripts/CatBoost/odyssey_v1.sh
@@ -22,5 +22,5 @@ for config in "${CONFIGS[@]}"; do
     cd ~/Master_Thesis/master-thesis-da/DNN_Trial
     source ~/miniconda3/etc/profile.d/conda.sh
     conda activate TabSurvey2
-    srun python3 train.py --config "$config" --model_name CatBoost --optimize_hyperparameters --n_trials 100  --epochs 100
+    srun python3 train.py --config "$config" --model_name CatBoost --optimize_hyperparameters --n_trials 99  --epochs 100
 done
