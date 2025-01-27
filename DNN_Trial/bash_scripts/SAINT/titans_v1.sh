2c3638a9af4eec2dda784948fb9c476c23e0ebf6
Anne Mburu <39761702+anniemburu@users.noreply.github.com>
2025-01-27 16:05:32 +0100

    Update titans_v1.sh

--- DNN_Trial/bash_scripts/SAINT/titans_v1.sh
+++ DNN_Trial/bash_scripts/SAINT/titans_v1.sh
@@ -21,7 +21,7 @@ for config in "${CONFIGS[@]}"; do
     cd ~/Master_Thesis/master-thesis-da/DNN_Trial
     source ~/miniconda3/etc/profile.d/conda.sh
     conda activate TabSurvey
-    srun python3 train.py --config "$config" --model_name SAINT --optimize_hyperparameters --n_trials 1 --epochs 100
+    srun python3 train.py --config "$config" --model_name SAINT --optimize_hyperparameters --n_trials 1 --epochs 100 --batch_size 64 --val_batch_size 128
 done
 
 
