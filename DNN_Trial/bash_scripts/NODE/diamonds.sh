30536fecbccc3c36862efe390b0d3ed6936ccaa9
Anne Mburu <39761702+anniemburu@users.noreply.github.com>
2025-02-25 12:09:19 +0100

    Update diamonds.sh

--- DNN_Trial/bash_scripts/NODE/diamonds.sh
+++ DNN_Trial/bash_scripts/NODE/diamonds.sh
@@ -10,6 +10,6 @@
 cd ~/Master_Thesis/master-thesis-da/DNN_Trial
 source ~/anaconda3/etc/profile.d/conda.sh
 conda activate Tab4Node
-srun python3 train.py --config config/diamonds.yml --model_name NODE --optimize_hyperparameters --n_trials 20 --epochs 100 --batch_size 64 --val_batch_size 128
+srun python3 train.py --config config/diamonds.yml --model_name NODE --optimize_hyperparameters --n_trials 20 --epochs 100 --batch_size 32 --val_batch_size 64
 
 
