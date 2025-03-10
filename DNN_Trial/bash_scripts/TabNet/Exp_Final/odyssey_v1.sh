877b7fad31f69f791ca208b2ebd5a57d8542671f
anniemburu <annemburu11@gmail.com>
2025-03-10 09:30:34 +0100

    Bash updates

---
+++ DNN_Trial/bash_scripts/TabNet/Exp_Final/odyssey_v1.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH --job-name=TabNet_Odyssey_V1
+#SBATCH --output=%x_%j.log
+#SBATCH --error=%x_%j.err
+#SBATCH --mail-user=mburu@uni-hildesheim.de
+#SBATCH --partition=STUD
+#SBATCH --gres=gpu:1
+
+CONFIGS=(
+    config/brazillian_houses.yml
+    config/abalone.yml
+    config/nyc_taxi.yml
+    config/house_sales.yml
+    config/mip_2016.yml
+)
+
+for config in "${CONFIGS[@]}"; do
+    printf "\n\n----------------------------------------------------------------------------\n"
+    printf 'Training TabNet Vesion 1 with Dataset: %s \n\n'  "$config" 
+    printf "\n\n----------------------------------------------------------------------------\n"
+
+    cd ~/Master_Thesis/master-thesis-da/DNN_Trial
+    source ~/anaconda3/etc/profile.d/conda.sh
+    conda activate TabSurvey
+    srun python3 train.py --config "$config" --model_name TabNet --optimize_hyperparameters
+done
