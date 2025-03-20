1035fc02bf64c6761df7f4683c9263ac3bd4c7cc
anniemburu <annemburu11@gmail.com>
2025-03-20 22:25:49 +0100

    Updated bash

--- DNN_Trial/bash_scripts/TabNet/Exp_Final/allstate.sh
+++ DNN_Trial/bash_scripts/TabNet/Exp_Final/allstate.sh
@@ -10,5 +10,5 @@
 cd ~/Master_Thesis/master-thesis-da/DNN_Trial
 source ~/anaconda3/etc/profile.d/conda.sh
 conda activate TabSurvey
-srun python3 train.py --config config/allstate.yml --model_name TabNet --optimize_hyperparameters --n_trials 5 --epochs 100
+srun python3 train.py --config config/allstate.yml --model_name TabNet --optimize_hyperparameters --n_trials 5 --epochs 100 --batch_size 64 --val_batch_size 128
 
