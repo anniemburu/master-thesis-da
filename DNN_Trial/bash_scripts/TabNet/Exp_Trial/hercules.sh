2ac04ae390bf85981ea02dfe828b63573d139790
Anne Mburu <39761702+anniemburu@users.noreply.github.com>
2025-04-10 21:25:08 +0200

    Create hercules.sh
    Updated bash

---
+++ DNN_Trial/bash_scripts/TabNet/Exp_Trial/hercules.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#SBATCH --job-name=TabNet_Hercules
+#SBATCH --output=%x_%j.log
+#SBATCH --error=%x_%j.err
+#SBATCH --mail-user=mburu@uni-hildesheim.de
+#SBATCH --partition=STUD
+#SBATCH --gres=gpu:1
+
+CONFIGS=(
+    config/abalone.yml
+    config/allstate.yml
+    #config/black_friday.yml
+    config/boston.yml
+    #config/brazillian_houses.yml
+    #config/diamonds.yml
+    #config/house_prices_nominal.yml
+    config/house_sales.yml
+    config/mercedes_benz.yml
+    config/mip_2016.yml
+    config/moneyball.yml
+    config/sat11.yml
+    config/sensory.yml
+    config/socmob.yml
+)
+
+for config in "${CONFIGS[@]}"; do
+    printf "\n\n----------------------------------------------------------------------------\n"
+    printf 'Training VIME Vesion 1 with Dataset: %s \n\n'  "$config" 
+    printf "\n\n----------------------------------------------------------------------------\n"
+
+    cd ~/Master_Thesis/master-thesis-da/DNN_Trial
+    source ~/anaconda3/etc/profile.d/conda.sh
+    conda activate TabSurvey
+    srun python3 train.py --config "$config" --model_name TabNet --objective probabilistic_regression
+
+done
