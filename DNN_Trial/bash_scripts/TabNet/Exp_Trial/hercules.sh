25fddcc2fe349d0205214596457e8c22ee5ac91f
anniemburu <annemburu11@gmail.com>
2025-04-14 12:00:18 +0200

    Modified bashes

--- DNN_Trial/bash_scripts/TabNet/Exp_Trial/hercules.sh
+++ DNN_Trial/bash_scripts/TabNet/Exp_Trial/hercules.sh
@@ -9,11 +9,11 @@
 CONFIGS=(
     config/abalone.yml
     config/allstate.yml
-    #config/black_friday.yml
+    config/black_friday.yml
     config/boston.yml
     #config/brazillian_houses.yml
-    #config/diamonds.yml
-    #config/house_prices_nominal.yml
+    config/diamonds.yml
+    config/house_prices_nominal.yml
     config/house_sales.yml
     config/mercedes_benz.yml
     config/mip_2016.yml
