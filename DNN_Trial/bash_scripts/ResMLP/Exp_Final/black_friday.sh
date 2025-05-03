fa40272be63c69ee0b775d45b6484748a71167a8
anniemburu <annemburu11@gmail.com>
2025-05-03 02:50:55 +0200

    Added bshr for new model

---
+++ DNN_Trial/bash_scripts/ResMLP/Exp_Final/black_friday.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name=ResMLP_BF_V1
+#SBATCH --output=%x_%j.log
+#SBATCH --error=%x_%j.err
+#SBATCH --mail-user=mburu@uni-hildesheim.de
+#SBATCH --partition=STUDL
+#SBATCH --gres=gpu:1
+#SBATCH --account=long
+
+
+cd ~/Master_Thesis/master-thesis-da/DNN_Trial
+source ~/anaconda3/etc/profile.d/conda.sh
+conda activate TabSurvey
+srun python3 train.py --config config/black_friday.yml --model_name ResMLP --optimize_hyperparameters --n_trials 5 --epochs 100
+
---
+++ DNN_Trial/bash_scripts/ResMLP/Exp_Final/titans_v1.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#SBATCH --job-name=ResMLP_Titans_V1
+#SBATCH --output=%x_%j.log
+#SBATCH --error=%x_%j.err
+#SBATCH --mail-user=mburu@uni-hildesheim.de
+#SBATCH --partition=STUDL
+#SBATCH --gres=gpu:1
+#SBATCH --account=long
+
+
+CONFIGS=(
+    "config/sat11.yml"
+    #"config/diamonds.yml"
+    "config/house_prices_nominal.yml"
+    "config/mercedes_benz.yml"
+    #"config/allstate.yml"
+    )
+
+for config in "${CONFIGS[@]}"; do
+    printf "\n\n----------------------------------------------------------------------------\n"
+    printf 'Training TabTransformer Vesion 1 with Dataset: %s \n\n'  "$config" 
+    printf "\n\n----------------------------------------------------------------------------\n"
+
+    cd ~/Master_Thesis/master-thesis-da/DNN_Trial
+    source ~/anaconda3/etc/profile.d/conda.sh
+    conda activate TabSurvey
+    srun python3 train.py --config "$config" --model_name ResMLP --optimize_hyperparameters --n_trials 5 --epochs 100
+done
+
+
+
