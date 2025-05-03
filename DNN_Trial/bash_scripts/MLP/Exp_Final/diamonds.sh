fab31a52b834da7f7f47fcb17f941df1f576b5ba
anniemburu <annemburu11@gmail.com>
2025-05-03 02:46:41 +0200

    Addes bash scripts for new model

---
+++ DNN_Trial/bash_scripts/MLP/Exp_Final/diamonds.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name=MLP_Diamonds_V1
+#SBATCH --output=%x_%j.log
+#SBATCH --error=%x_%j.err
+#SBATCH --mail-user=mburu@uni-hildesheim.de
+#SBATCH --partition=STUDL
+#SBATCH --gres=gpu:1
+#SBATCH --account=long
+
+
+cd ~/Master_Thesis/master-thesis-da/DNN_Trial
+source ~/anaconda3/etc/profile.d/conda.sh
+conda activate TabSurvey
+srun python3 train.py --config config/diamonds.yml --model_name MLP --optimize_hyperparameters --n_trials 8 --epochs 100 --batch_size 64 --val_batch_size 128
+
