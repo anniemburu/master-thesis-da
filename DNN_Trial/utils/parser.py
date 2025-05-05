a575dba34334f4abbdc0626fac045e8a314b2dc9
Anne Mburu <annemburu11@gmail.com>
2025-05-05 15:42:12 +0200

    Updated

--- DNN_Trial/utils/parser.py
+++ DNN_Trial/utils/parser.py
@@ -40,6 +40,7 @@ def get_parser():
     parser.add('--one_hot_encode', action="store_true", help="OneHotEncode the categorical features")
     parser.add('--ordinal_encode', action="store_true", help="Encode Ordinal categorical features")
     parser.add('--frequency_reg', action="store_true", help="Perform frequency regularization")
+    parser.add('--class_comp', action='store_false', default=False, help='Convert Classification to Regression')
 
     parser.add('--batch_size', type=int, default=128, help="Batch size used for training")
     parser.add('--val_batch_size', type=int, default=128, help="Batch size used for training and testing")
