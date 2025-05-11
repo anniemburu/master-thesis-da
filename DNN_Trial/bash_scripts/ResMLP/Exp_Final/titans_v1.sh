9e3b50b05bce06d691f7c4b7dd38caea6f5b50fb
Anne Mburu <annemburu11@gmail.com>
2025-05-11 03:05:37 +0200

    Updates

--- DNN_Trial/bash_scripts/ResMLP/Exp_Final/titans_v1.sh
+++ DNN_Trial/bash_scripts/ResMLP/Exp_Final/titans_v1.sh
@@ -10,10 +10,8 @@
 
 CONFIGS=(
     "config/sat11.yml"
-    #"config/diamonds.yml"
     "config/house_prices_nominal.yml"
     "config/mercedes_benz.yml"
-    #"config/allstate.yml"
     )
 
 for config in "${CONFIGS[@]}"; do
