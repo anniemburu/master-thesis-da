93bda80090a4f899e45c86b8c9315fdfd17d0bde
Anne Mburu <annemburu11@gmail.com>
2025-05-15 09:38:23 +0200

    Try to double train

--- DNN_Trial/bash_scripts/ResMLP/Exp_Trial/hercules.sh
+++ DNN_Trial/bash_scripts/ResMLP/Exp_Trial/hercules.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=ResMLP_Hercules_reg
+#SBATCH --job-name=ResMLP_Hercules_q_binorms
 #SBATCH --output=%x_%j.log
 #SBATCH --error=%x_%j.err
 #SBATCH --mail-user=mburu@uni-hildesheim.de
@@ -8,20 +8,20 @@
 #SBATCH --account=long
 
 CONFIGS=(
-    config/abalone.yml
-    config/allstate.yml
-    config/black_friday.yml
-    config/boston.yml
-    config/brazillian_houses.yml
-    config/diamonds.yml
-    config/house_prices_nominal.yml
-    config/house_sales.yml
+    #config/abalone.yml
+    #config/allstate.yml
+    #config/black_friday.yml
+    #config/boston.yml
+    #config/brazillian_houses.yml
+    #config/diamonds.yml
+    #config/house_prices_nominal.yml
+    #config/house_sales.yml
     config/mercedes_benz.yml
     config/mip_2016.yml
-    config/moneyball.yml
+    #config/moneyball.yml
     config/sat11.yml
-    config/sensory.yml
-    config/socmob.yml
+    #config/sensory.yml
+    #config/socmob.yml
 )
 
 for config in "${CONFIGS[@]}"; do
@@ -32,7 +32,9 @@ for config in "${CONFIGS[@]}"; do
     cd ~/Master_Thesis/master-thesis-da/DNN_Trial
     source ~/anaconda3/etc/profile.d/conda.sh
     conda activate TabSurvey
-    #srun python3 train.py --config "$config" --model_name ResMLP --objective probabilistic_regression --class_comp --batch_size 64 --val_batch_size 128
-    srun python3 train.py --config "$config" --model_name ResMLP --objective regression --batch_size 64 --val_batch_size 128
+    srun python3 train.py --config "$config" --model_name ResMLP --objective probabilistic_regression --strategy quantile --class_comp --batch_size 64 --val_batch_size 128
+    srun python3 train.py --config "$config" --model_name ResMLP --objective probabilistic_regression --strategy uniform --class_comp --batch_size 64 --val_batch_size 128
+
+    #srun python3 train.py --config "$config" --model_name ResMLP --objective regression --batch_size 64 --val_batch_size 128
 
 done
