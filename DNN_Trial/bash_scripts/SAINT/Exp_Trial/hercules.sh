89e4539a041a6ab1438f45e9f6bf214a7799bf5b
Anne Mburu <annemburu11@gmail.com>
2025-05-18 23:29:36 +0200

    Updates script

--- DNN_Trial/bash_scripts/SAINT/Exp_Trial/hercules.sh
+++ DNN_Trial/bash_scripts/SAINT/Exp_Trial/hercules.sh
@@ -36,9 +36,9 @@ for config in "${CONFIGS[@]}"; do
     srun python3 train.py --config "$config" --model_name SAINT --objective probabilistic_regression --strategy kmeans --class_comp --batch_size 64 --val_batch_size 128
     srun python3 train.py --config "$config" --model_name SAINT --objective probabilistic_regression --strategy uniform --class_comp --batch_size 64 --val_batch_size 128
 
-    srun python3 train.py --config "$config" --model_name SAINT --objective probabilistic_regression --strategy quantile --class_comp --weighted_loss --batch_size 64 --val_batch_size 128
-    srun python3 train.py --config "$config" --model_name SAINT --objective probabilistic_regression --strategy kmeans --class_comp --weighted_loss --batch_size 64 --val_batch_size 128
-    srun python3 train.py --config "$config" --model_name SAINT --objective probabilistic_regression --strategy uniform --class_comp --weighted_loss --batch_size 64 --val_batch_size 128
+    #srun python3 train.py --config "$config" --model_name SAINT --objective probabilistic_regression --strategy quantile --class_comp --weighted_loss --batch_size 64 --val_batch_size 128
+    #srun python3 train.py --config "$config" --model_name SAINT --objective probabilistic_regression --strategy kmeans --class_comp --weighted_loss --batch_size 64 --val_batch_size 128
+    #srun python3 train.py --config "$config" --model_name SAINT --objective probabilistic_regression --strategy uniform --class_comp --weighted_loss --batch_size 64 --val_batch_size 128
 
     #srun python3 train.py --config "$config" --model_name SAINT --objective regression --batch_size 64 --val_batch_size 128
 
