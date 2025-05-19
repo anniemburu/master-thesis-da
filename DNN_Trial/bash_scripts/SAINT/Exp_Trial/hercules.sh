16b81d594b75e946aae805877cf82de01a56dcd4
Anne Mburu <annemburu11@gmail.com>
2025-05-19 15:03:22 +0200

    Updates on Freedmans

--- DNN_Trial/bash_scripts/SAINT/Exp_Trial/hercules.sh
+++ DNN_Trial/bash_scripts/SAINT/Exp_Trial/hercules.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=SAINT_Hercules_all_struges
+#SBATCH --job-name=SAINT_Hercules_all_freedmans
 #SBATCH --output=%x_%j.log
 #SBATCH --error=%x_%j.err
 #SBATCH --mail-user=mburu@uni-hildesheim.de
