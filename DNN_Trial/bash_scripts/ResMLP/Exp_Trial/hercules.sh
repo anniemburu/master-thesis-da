b30caa76c707e9e27fc144ed52a9b8a9c46f7ab2
Anne Mburu <39761702+anniemburu@users.noreply.github.com>
2025-05-20 08:45:31 +0200

    Update hercules.sh

--- DNN_Trial/bash_scripts/ResMLP/Exp_Trial/hercules.sh
+++ DNN_Trial/bash_scripts/ResMLP/Exp_Trial/hercules.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=ResMLP_Hercules_weighted_struges
+#SBATCH --job-name=ResMLP_Hercules_weighted_struges_ntest
 #SBATCH --output=%x_%j.log
 #SBATCH --error=%x_%j.err
 #SBATCH --mail-user=mburu@uni-hildesheim.de
@@ -37,9 +37,9 @@ for config in "${CONFIGS[@]}"; do
     srun python3 train.py --config "$config" --model_name ResMLP --objective probabilistic_regression --strategy uniform --class_comp --batch_size 64 --val_batch_size 128
 
 
-    srun python3 train.py --config "$config" --model_name ResMLP --objective probabilistic_regression --strategy quantile --class_comp --weighted_loss --batch_size 64 --val_batch_size 128
-    srun python3 train.py --config "$config" --model_name ResMLP --objective probabilistic_regression --strategy kmeans --class_comp --weighted_loss --batch_size 64 --val_batch_size 128
-    srun python3 train.py --config "$config" --model_name ResMLP --objective probabilistic_regression --strategy uniform --class_comp --weighted_loss --batch_size 64 --val_batch_size 128
+    #srun python3 train.py --config "$config" --model_name ResMLP --objective probabilistic_regression --strategy quantile --class_comp --weighted_loss --batch_size 64 --val_batch_size 128
+    #srun python3 train.py --config "$config" --model_name ResMLP --objective probabilistic_regression --strategy kmeans --class_comp --weighted_loss --batch_size 64 --val_batch_size 128
+    #srun python3 train.py --config "$config" --model_name ResMLP --objective probabilistic_regression --strategy uniform --class_comp --weighted_loss --batch_size 64 --val_batch_size 128
 
     #srun python3 train.py --config "$config" --model_name ResMLP --objective regression --batch_size 64 --val_batch_size 128
 
