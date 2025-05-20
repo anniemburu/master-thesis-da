7c7d1629101b02b055e12842d103ec30052a3833
Anne Mburu <annemburu11@gmail.com>
2025-05-21 01:56:50 +0200

    Reverted naming

---
+++ DNN_Trial/bash_scripts/FTTransformer/Trials/hercules.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+#SBATCH --job-name=FTTransformer_Hercules_all_struges
+#SBATCH --output=%x_%j.log
+#SBATCH --error=%x_%j.err
+#SBATCH --mail-user=mburu@uni-hildesheim.de
+#SBATCH --partition=STUD
+#SBATCH --gres=gpu:1
+
+
+CONFIGS=(
+    config/abalone.yml
+    config/allstate.yml
+    config/black_friday.yml
+    config/boston.yml
+    config/brazillian_houses.yml
+    config/diamonds.yml
+    config/house_prices_nominal.yml
+    config/house_sales.yml
+    config/mercedes_benz.yml
+    config/mip_2016.yml
+    config/moneyball.yml
+    config/sat11.yml
+    config/sensory.yml
+    config/socmob.yml
+)
+
+for config in "${CONFIGS[@]}"; do
+    printf "\n\n----------------------------------------------------------------------------\n"
+    printf 'Training FTTransformer Vesion 1 with Dataset: %s \n\n'  "$config" 
+    printf "\n\n----------------------------------------------------------------------------\n"
+
+    cd ~/Master_Thesis/master-thesis-da/DNN_Trial
+    source ~/anaconda3/etc/profile.d/conda.sh
+    conda activate TabSurvey
+
+    srun python3 train.py --config "$config" --model_name FTTransformer --objective probabilistic_regression --strategy quantile --class_comp  --batch_size 16 --val_batch_size 32
+    srun python3 train.py --config "$config" --model_name FTTransformer --objective probabilistic_regression --strategy quantile --class_comp --weighted_loss --batch_size 16 --val_batch_size 32
+
+    srun python3 train.py --config "$config" --model_name FTTransformer --objective probabilistic_regression --strategy kmeans --class_comp  --batch_size 16 --val_batch_size 32
+    srun python3 train.py --config "$config" --model_name FTTransformer --objective probabilistic_regression --strategy kmeans --class_comp --weighted_loss --batch_size 16 --val_batch_size 32
+
+    srun python3 train.py --config "$config" --model_name FTTransformer --objective probabilistic_regression --strategy uniform --class_comp  --batch_size 16 --val_batch_size 32
+    srun python3 train.py --config "$config" --model_name FTTransformer --objective probabilistic_regression --strategy uniform --class_comp --weighted_loss --batch_size 16 --val_batch_size 32
+
+    #srun python3 train.py --config "$config" --model_name FTTransformer --objective regression --batch_size 16 --val_batch_size 32
+
+done
