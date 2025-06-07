93b5d948f8c176fde4c293a85f75b09ac660f802
Anne Mburu <annemburu11@gmail.com>
2025-06-07 20:19:09 +0200

    Viz scripts

--- DNN_Trial/utils/load_data.py
+++ DNN_Trial/utils/load_data.py
@@ -46,7 +46,8 @@ def load_data(args, is_test=False):
 
     ####~~~~~~~~~~~~~~~~~~~~ SPECIFY HOW DATASETS ARE LOADED AND STUFF ~~~~~~~~~~~~~~~~~ 
     if args.dataset == "Boston":
-        df = pd.read_csv('/home/mburu/Master_Thesis/master-thesis-da/datasets/531-boston/raw_data.csv')
+        #df = pd.read_csv('/home/mburu/Master_Thesis/master-thesis-da/datasets/531-boston/raw_data.csv')
+        #df = pd.read_csv('/home/mburu/Master_Thesis/master-thesis-da/datasets/531-boston/raw_data.csv')
         #df = pd.read_csv('/Users/wambo/Desktop/Master Thesis/master-thesis-da/datasets/531-boston.csv')
         #df = pd.read_csv('/home/wambo/Desktop/Master Thesis/datasets/531-boston.csv')
         #df = pd.read_csv('/Users/johnmburu/Desktop/Master Thesis/master-thesis-da/datasets/531-boston.csv')
@@ -60,7 +61,8 @@ def load_data(args, is_test=False):
         y = df[label_col].to_numpy()
 
     elif args.dataset == "Socmob":
-        df = pd.read_csv('/home/mburu/Master_Thesis/master-thesis-da/datasets/541-socmob/raw_data.csv') #CLUSTER
+        #df = pd.read_csv('/home/mburu/Master_Thesis/master-thesis-da/datasets/541-socmob/raw_data.csv') #CLUSTER
+        #df = pd.read_csv('/home/mburu/Master_Thesis/master-thesis-da/datasets/541-socmob/raw_data.csv') #CLUSTER
         #df = pd.read_csv('/Users/wambo/Desktop/Master Thesis/master-thesis-da/datasets/541-socmob.csv')
         #df = pd.read_csv('/home/wambo/Desktop/Master Thesis/datasets/541-socmob.csv') #UBUNTU
         #df = pd.read_csv('/Users/johnmburu/Desktop/Master Thesis/master-thesis-da/datasets/541-socmob.csv')
@@ -74,7 +76,8 @@ def load_data(args, is_test=False):
         y = df[label_col].to_numpy()
 
     elif args.dataset == "Sensory":
-        df = pd.read_csv('/home/mburu/Master_Thesis/master-thesis-da/datasets/546-sensory/raw_data.csv') #CLUSTER
+        #df = pd.read_csv('/home/mburu/Master_Thesis/master-thesis-da/datasets/546-sensory/raw_data.csv') #CLUSTER
+        #df = pd.read_csv('/home/mburu/Master_Thesis/master-thesis-da/datasets/546-sensory/raw_data.csv') #CLUSTER
         #df = pd.read_csv('/Users/wambo/Desktop/Master Thesis/master-thesis-da/datasets/546-sensory.csv')
         #df = pd.read_csv('/home/wambo/Desktop/Master Thesis/datasets/546-sensory.csv')
         #df = pd.read_csv('/Users/johnmburu/Desktop/Master Thesis/master-thesis-da/datasets/546-sensory.csv')
@@ -88,7 +91,8 @@ def load_data(args, is_test=False):
         y = df[label_col].to_numpy()
 
     elif args.dataset == "Moneyball":
-        df = pd.read_csv('/home/mburu/Master_Thesis/master-thesis-da/datasets/41021-Moneyball/raw_data.csv') #CLUSTER
+        #df = pd.read_csv('/home/mburu/Master_Thesis/master-thesis-da/datasets/41021-Moneyball/raw_data.csv') #CLUSTER
+        #df = pd.read_csv('/home/mburu/Master_Thesis/master-thesis-da/datasets/41021-Moneyball/raw_data.csv') #CLUSTER
         #df = pd.read_csv('/Users/wambo/Desktop/Master Thesis/master-thesis-da/datasets/41021-Moneyball.csv')
         #df = pd.read_csv('/home/wambo/Desktop/Master Thesis/datasets/41021-Moneyball.csv')
         #df = pd.read_csv('/Users/johnmburu/Desktop/Master Thesis/master-thesis-da/datasets/41021-Moneyball.csv')
@@ -108,7 +112,8 @@ def load_data(args, is_test=False):
         y = df[label_col].to_numpy()
 
     elif args.dataset == "Black_Friday" :
-        df = pd.read_csv('/home/mburu/Master_Thesis/master-thesis-da/datasets/41540-black_friday/raw_data.csv') #CLUSTER
+        #df = pd.read_csv('/home/mburu/Master_Thesis/master-thesis-da/datasets/41540-black_friday/raw_data.csv') #CLUSTER
+        #df = pd.read_csv('/home/mburu/Master_Thesis/master-thesis-da/datasets/41540-black_friday/raw_data.csv') #CLUSTER
         #df = pd.read_csv('/Users/wambo/Desktop/Master Thesis/master-thesis-da/datasets/41540-black_friday.csv')
         #df = pd.read_csv('/home/wambo/Desktop/Master Thesis/datasets/41540-black_friday.csv')
         #df = pd.read_csv('/Users/johnmburu/Desktop/Master Thesis/master-thesis-da/datasets/41540-black_friday.csv')
@@ -136,7 +141,8 @@ def load_data(args, is_test=False):
 
 
     elif args.dataset == "SAT11":
-        df = pd.read_csv('/home/mburu/Master_Thesis/master-thesis-da/datasets/41980-SAT11-HAND-runtime-regression/raw_data.csv') #CLUSTER
+        #df = pd.read_csv('/home/mburu/Master_Thesis/master-thesis-da/datasets/41980-SAT11-HAND-runtime-regression/raw_data.csv') #CLUSTER
+        #df = pd.read_csv('/home/mburu/Master_Thesis/master-thesis-da/datasets/41980-SAT11-HAND-runtime-regression/raw_data.csv') #CLUSTER
         #df = pd.read_csv('/Users/wambo/Desktop/Master Thesis/master-thesis-da/datasets/41980-SAT11-HAND-runtime-regression.csv')
         #df = pd.read_csv('/home/wambo/Desktop/Master Thesis/datasets/41980-SAT11-HAND-runtime-regression.csv')
         #df = pd.read_csv('/Users/johnmburu/Desktop/Master Thesis/master-thesis-da/datasets/41980-SAT11-HAND-runtime-regression.csv')
@@ -156,7 +162,8 @@ def load_data(args, is_test=False):
         y = df[label_col].to_numpy()
 
     elif args.dataset == "Diamonds":
-        df = pd.read_csv('/home/mburu/Master_Thesis/master-thesis-da/datasets/42225-diamonds/raw_data.csv') #CLUSTER
+        #df = pd.read_csv('/home/mburu/Master_Thesis/master-thesis-da/datasets/42225-diamonds/raw_data.csv') #CLUSTER
+        #df = pd.read_csv('/home/mburu/Master_Thesis/master-thesis-da/datasets/42225-diamonds/raw_data.csv') #CLUSTER
         #df = pd.read_csv('/Users/wambo/Desktop/Master Thesis/master-thesis-da/datasets/42225-diamonds.csv')
         #df = pd.read_csv('/home/wambo/Desktop/Master Thesis/datasets/42225-diamonds.csv')
         #df = pd.read_csv('/Users/johnmburu/Desktop/Master Thesis/master-thesis-da/datasets/42225-diamonds.csv')
@@ -170,7 +177,8 @@ def load_data(args, is_test=False):
         y = df[label_col].to_numpy()
 
     elif args.dataset == "House_Prices_Nominal":
-        df = pd.read_csv('/home/mburu/Master_Thesis/master-thesis-da/datasets/42563-house_prices_nominal/raw_data.csv') #CLUSTER
+        #df = pd.read_csv('/home/mburu/Master_Thesis/master-thesis-da/datasets/42563-house_prices_nominal/raw_data.csv') #CLUSTER
+        #df = pd.read_csv('/home/mburu/Master_Thesis/master-thesis-da/datasets/42563-house_prices_nominal/raw_data.csv') #CLUSTER
         #df = pd.read_csv('/Users/wambo/Desktop/Master Thesis/master-thesis-da/datasets/42563-house_prices_nominal.csv')
         #df = pd.read_csv('/home/wambo/Desktop/Master Thesis/datasets/42563-house_prices_nominal.csv')
         #df = pd.read_csv('/Users/johnmburu/Desktop/Master Thesis/master-thesis-da/datasets/42563-house_prices_nominal.csv')
@@ -204,7 +212,8 @@ def load_data(args, is_test=False):
         y = df[label_col].to_numpy()
 
     elif args.dataset == "Mercedes_Benz":
-        df = pd.read_csv('/home/mburu/Master_Thesis/master-thesis-da/datasets/42570-Mercedes_Benz_Greener_Manufacturing/raw_data.csv') #CLUSTER
+        #df = pd.read_csv('/home/mburu/Master_Thesis/master-thesis-da/datasets/42570-Mercedes_Benz_Greener_Manufacturing/raw_data.csv') #CLUSTER
+        #df = pd.read_csv('/home/mburu/Master_Thesis/master-thesis-da/datasets/42570-Mercedes_Benz_Greener_Manufacturing/raw_data.csv') #CLUSTER
         #df = pd.read_csv('/Users/wambo/Desktop/Master Thesis/master-thesis-da/datasets/42570-Mercedes_Benz_Greener_Manufacturing.csv')
         #df = pd.read_csv('/home/wambo/Desktop/Master Thesis/datasets/42570-Mercedes_Benz_Greener_Manufacturing.csv')
         #df = pd.read_csv('/Users/johnmburu/Desktop/Master Thesis/master-thesis-da/datasets/42570-Mercedes_Benz_Greener_Manufacturing.csv')
@@ -224,7 +233,8 @@ def load_data(args, is_test=False):
         y = df[label_col].to_numpy()
 
     elif args.dataset == "Allstate_Claims":
-        df = pd.read_csv('/home/mburu/Master_Thesis/master-thesis-da/datasets/42571-Allstate_Claims_Severity/raw_data.csv') #CLUSTER
+        #df = pd.read_csv('/home/mburu/Master_Thesis/master-thesis-da/datasets/42571-Allstate_Claims_Severity/raw_data.csv') #CLUSTER
+        #df = pd.read_csv('/home/mburu/Master_Thesis/master-thesis-da/datasets/42571-Allstate_Claims_Severity/raw_data.csv') #CLUSTER
         #df = pd.read_csv('/Users/wambo/Desktop/Master Thesis/master-thesis-da/datasets/42571-Allstate_Claims_Severity.csv')
         #df = pd.read_csv('/home/wambo/Desktop/Master Thesis/datasets/42571-Allstate_Claims_Severity.csv')
         #df = pd.read_csv('/Users/johnmburu/Desktop/Master Thesis/master-thesis-da/datasets/42571-Allstate_Claims_Severity.csv')
@@ -244,7 +254,8 @@ def load_data(args, is_test=False):
         y = df[label_col].to_numpy()
 
     elif args.dataset == "Brazillian_Houses":
-        df = pd.read_csv('/home/mburu/Master_Thesis/master-thesis-da/datasets/42688-Brazilian_houses/raw_data.csv') #CLUSTER
+        #df = pd.read_csv('/home/mburu/Master_Thesis/master-thesis-da/datasets/42688-Brazilian_houses/raw_data.csv') #CLUSTER
+        #df = pd.read_csv('/home/mburu/Master_Thesis/master-thesis-da/datasets/42688-Brazilian_houses/raw_data.csv') #CLUSTER
         #df = pd.read_csv('/Users/wambo/Desktop/Master Thesis/master-thesis-da/datasets/42688-Brazilian_houses.csv')
         #df = pd.read_csv('/home/wambo/Desktop/Master Thesis/datasets/42688-Brazilian_houses.csv')
         #df = pd.read_csv('/Users/johnmburu/Desktop/Master Thesis/master-thesis-da/datasets/42688-Brazilian_houses.csv')
@@ -258,7 +269,8 @@ def load_data(args, is_test=False):
         y = df[label_col].to_numpy()
 
     elif args.dataset == "Abalone":
-        df = pd.read_csv('/home/mburu/Master_Thesis/master-thesis-da/datasets/42726-abalone/raw_data.csv') #CLUSTER
+        #df = pd.read_csv('/home/mburu/Master_Thesis/master-thesis-da/datasets/42726-abalone/raw_data.csv') #CLUSTER
+        #df = pd.read_csv('/home/mburu/Master_Thesis/master-thesis-da/datasets/42726-abalone/raw_data.csv') #CLUSTER
         #df = pd.read_csv('/Users/wambo/Desktop/Master Thesis/master-thesis-da/datasets/42726-abalone.csv')
         #df = pd.read_csv('/home/wambo/Desktop/Master Thesis/datasets/42726-abalone.csv')
         #df = pd.read_csv('/Users/johnmburu/Desktop/Master Thesis/master-thesis-da/datasets/42726-abalone.csv')
@@ -290,7 +302,8 @@ def load_data(args, is_test=False):
         y = df[label_col].to_numpy()
 
     elif args.dataset == "House_Sales":
-        df = pd.read_csv('/home/mburu/Master_Thesis/master-thesis-da/datasets/42731-house_sales/raw_data.csv') #CLUSTER
+        #df = pd.read_csv('/home/mburu/Master_Thesis/master-thesis-da/datasets/42731-house_sales/raw_data.csv') #CLUSTER
+        #df = pd.read_csv('/home/mburu/Master_Thesis/master-thesis-da/datasets/42731-house_sales/raw_data.csv') #CLUSTER
         #df = pd.read_csv('/Users/wambo/Desktop/Master Thesis/master-thesis-da/datasets/42731-house_sales.csv')
         #df = pd.read_csv('/home/wambo/Desktop/Master Thesis/datasets/42731-house_sales.csv')
         #df = pd.read_csv('/Users/johnmburu/Desktop/Master Thesis/master-thesis-da/datasets/42731-house_sales.csv')
@@ -310,7 +323,8 @@ def load_data(args, is_test=False):
         y = df[label_col].to_numpy()
 
     elif args.dataset == "MIP":
-        df = pd.read_csv('/home/mburu/Master_Thesis/master-thesis-da/datasets/43071-MIP-2016-regression/raw_data.csv') #CLUSTER
+        #df = pd.read_csv('/home/mburu/Master_Thesis/master-thesis-da/datasets/43071-MIP-2016-regression/raw_data.csv') #CLUSTER
+        #df = pd.read_csv('/home/mburu/Master_Thesis/master-thesis-da/datasets/43071-MIP-2016-regression/raw_data.csv') #CLUSTER
         #df = pd.read_csv('/Users/wambo/Desktop/Master Thesis/master-thesis-da/datasets/43071-MIP-2016-regression.csv')
         #df = pd.read_csv('/home/wambo/Desktop/Master Thesis/datasets/3071-MIP-2016-regression.csv')
         #df = pd.read_csv('/Users/johnmburu/Desktop/Master Thesis/master-thesis-da/datasets/43071-MIP-2016-regression.csv')
