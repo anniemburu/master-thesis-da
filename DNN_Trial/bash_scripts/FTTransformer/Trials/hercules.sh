6398e936234d0fb70bb6906340c7c98453cfc232
Anne Mburu <39761702+anniemburu@users.noreply.github.com>
2025-06-08 12:51:05 +0200

    Update hercules.sh

--- DNN_Trial/bash_scripts/FTTransformer/Trials/hercules.sh
+++ DNN_Trial/bash_scripts/FTTransformer/Trials/hercules.sh
@@ -9,14 +9,14 @@
 
 CONFIGS=(
     config/abalone.yml
-    config/allstate.yml
-    config/black_friday.yml
+    #config/allstate.yml
+    #config/black_friday.yml
     config/boston.yml
     config/brazillian_houses.yml
     config/diamonds.yml
-    config/house_prices_nominal.yml
-    config/house_sales.yml
-    config/mercedes_benz.yml
+    #config/house_prices_nominal.yml
+    #config/house_sales.yml
+    #config/mercedes_benz.yml
     config/mip_2016.yml
     config/moneyball.yml
     config/sat11.yml
