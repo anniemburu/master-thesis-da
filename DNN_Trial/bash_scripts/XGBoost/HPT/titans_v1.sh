219f512b8a8c564011a2b74574246389f4ef94d3
Anne Mburu <39761702+anniemburu@users.noreply.github.com>
2025-06-17 15:23:03 +0200

    Update titans_v1.sh

--- DNN_Trial/bash_scripts/XGBoost/HPT/titans_v1.sh
+++ DNN_Trial/bash_scripts/XGBoost/HPT/titans_v1.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=XGBoost_Titans_final_reg
 #SBATCH --output=%x_%j.log
 #SBATCH --error=%x_%j.err
-#SBATCH --mail-user=mburu@uni-hildesheim.de
+#SBATCH --mail-user=wekesa@uni-hildesheim.de
 #SBATCH --partition=STUD
 #SBATCH --gres=gpu:1
 
